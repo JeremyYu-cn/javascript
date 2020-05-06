545f978c668450fc50c025d485f0bd712ff0aff3
Yzm <54796446@qq.com>
2020-05-06 17:11:45 +0800

    add selection sort and inertion sort

--- __tests__/my.test.js
+++ __tests__/my.test.js
@@ -23,5 +23,15 @@ describe('find and sort', () => {
         expect(sort.QuickSort(arr)).toEqual([0,1,2,7])
     })
 
+    test('selection sort', () => {
+        let arr = [11,21,31,51,41,11,21];
+        expect(sort.SelectionSort(arr)).toEqual([11,11,21,21,31,41,51])
+    })
+
+    test('insertion sort', () => {
+        let arr = [3,2,1,5,4,8,6,7];
+        expect(sort.InsertSort(arr)).toEqual([1,2,3,4,5,6,7,8]);
+    })
+
 })
 
--- sort/index.js
+++ sort/index.js
@@ -52,6 +52,39 @@
         }
         return [].concat(left, [basic], right);
     }
+
+    // 选择排序
+    SelectionSort(arr) {
+        const len = arr.length;
+        for (let i = 0; i < len; i++) {
+            let minIndex = i;
+            for(let j = i + 1; j < len; j++) {
+                minIndex = arr[minIndex] <= arr[j] ? minIndex : j;
+            }
+            if (minIndex !== i) {
+                let tmp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = tmp;
+            }
+        }
+        return arr;
+    }
+
+    // 插入排序
+    InsertSort(arr) {
+        const len = arr.length;
+        let result = [];
+        result.push(arr[0]);
+        for(let i = 1; i < len; i++) {
+            let tmpIndex = 0;
+            for (let j = 0; j < result.length; j++) {
+                tmpIndex = j;
+                if (result[tmpIndex] > arr[i]){ break; } else { tmpIndex++; }
+            }
+            result.splice(tmpIndex, 0, arr[i]);
+        }
+        return result;
+    }
  }
 
 module.exports = SortMethod;
